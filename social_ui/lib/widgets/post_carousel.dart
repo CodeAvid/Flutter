ff4e796208ef86cd5b337a67098bd574c925c082
simplybenjmain2.0@gmail.com <simplybenjamin2.0@gmail.com>
2021-05-22 12:35:07 +0100

    Add page controller animation

--- social_ui/lib/widgets/post_carousel.dart
+++ social_ui/lib/widgets/post_carousel.dart
@@ -15,110 +15,126 @@ class PostCarousel extends StatelessWidget {
 
   _buildPost(context, index) {
     Post post = posts[index];
-    return Stack(
-      children: [
-        Container(
-          margin: const EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                offset: Offset(0, 2),
-                blurRadius: 6.0,
-              )
-            ],
+    return AnimatedBuilder(
+      animation: pageController,
+      builder: (BuildContext context, Widget widget) {
+        double value = 0.0;
+        if (pageController.position.haveDimensions) {
+          value = pageController.page - index;
+          value = (1 - (value.abs() * 0.25)).clamp(0.1, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 400.0,
+            child: widget,
           ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
-            child: Image(
-              width: 300.0,
-              height: 400,
-              image: AssetImage(post.imageUrl),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          left: 10,
-          right: 10,
-          child: Container(
-            padding: const EdgeInsets.all(12.0),
-            height: 110.0,
+        );
+      },
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10.0),
             decoration: BoxDecoration(
-              color: Colors.white54,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(15.0),
-                bottomRight: Radius.circular(15.0),
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Post ${index.toString()}',
-                  style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.width / 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  'Location ${index.toString()}',
-                  style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.width / 28,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 4.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                          size: 18,
-                        ),
-                        SizedBox(width: 6.0),
-                        Text(
-                          '${post.likes.toString()}',
-                          style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.width / 32,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.chat,
-                          color: Theme.of(context).primaryColor,
-                          size: 18,
-                        ),
-                        SizedBox(width: 6.0),
-                        Text(
-                          '${post.comments.toString()}',
-                          style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.width / 32,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  offset: Offset(0, 2),
+                  blurRadius: 6.0,
+                )
               ],
             ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                width: 300.0,
+                height: 400,
+                image: AssetImage(post.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        )
-      ],
+          Positioned(
+            bottom: 0,
+            left: 10,
+            right: 10,
+            child: Container(
+              padding: const EdgeInsets.all(12.0),
+              height: 110.0,
+              decoration: BoxDecoration(
+                color: Colors.white54,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Post ${index.toString()}',
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width / 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    'Location ${index.toString()}',
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width / 28,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 4.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                            size: 18,
+                          ),
+                          SizedBox(width: 6.0),
+                          Text(
+                            '${post.likes.toString()}',
+                            style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.width / 32,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.chat,
+                            color: Theme.of(context).primaryColor,
+                            size: 18,
+                          ),
+                          SizedBox(width: 6.0),
+                          Text(
+                            '${post.comments.toString()}',
+                            style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.width / 32,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
